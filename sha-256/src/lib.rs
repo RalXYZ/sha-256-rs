a07340158243c03476bf35138a8cf36893d30ef9
RalXYZ <RalXYZ@pm.me>
2021-09-14 11:47:29 +0800

    add comments, use Sha256 trait

--- sha-256/src/lib.rs
+++ sha-256/src/lib.rs
@@ -3,6 +3,8 @@
 use std::convert::TryInto;
 use std::ops::{Div, Rem};
 
+// Initialize array of round constants:
+// (first 32 bits of the fractional parts of the cube roots of the first 64 primes 2..311):
 const K: [u32; 64] = [
     0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
     0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
@@ -14,23 +16,33 @@ const K: [u32; 64] = [
     0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
 ];
 
-pub struct Sha256 {}
-
-impl Default for Sha256 {
-    fn default() -> Self {
-        todo!()
-    }
+pub trait Sha256 {
+    fn do_hash(&self) -> [u8; 32];
+    fn process_64_byte(input: [u8; 64], h: &mut [u32; 8]);
+    fn right_rotate(n: u32, d: u32) -> u32;
 }
 
-impl Sha256 {
-    pub fn do_hash(&mut self, data: &[u8]) -> [u8; 32] {
+impl Sha256 for Vec<u8> {
+    fn do_hash(&self) -> [u8; 32] {
+
+        // Initialize hash values:
+        // (first 32 bits of the fractional parts of the square roots of the first 8 primes 2..19):
         let mut h: [u32; 8] = [
             0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab,
             0x5be0cd19,
         ];
 
-        let input_len = data.len();
-        let mut data: Vec<u8> = data.to_vec();
+        // Pre-processing (Padding):
+        // begin with the original message of length L bits
+        // append a single '1' bit
+        // append K '0' bits, where K is the minimum number >= 0
+        // such that L + 1 + K + 64 is a multiple of 512
+        // append L as a 64-bit big-endian integer,
+        // making the total post-processed length a multiple of 512 bits
+        // such that the bits in the message are
+        // L 1 00..<K 0's>..00 <L as 64 bit integer> = k*512 total bits
+        let input_len = self.len();
+        let mut data: Vec<u8> = self.to_vec();
         data.push(0b1000_0000);
         if data.len().rem(64).lt(&56) {
             data.append(&mut vec![0u8; 56 - (data.len().rem(64))]);
@@ -39,6 +51,8 @@ impl Sha256 {
         }
         data.extend_from_slice(&(input_len * 8).to_be_bytes());
 
+        // Process the message in successive 512-bit chunks:
+        // break message into 512-bit chunks
         let data: Vec<[u8; 64]> = (0..(data.len().div(64)))
             .map(|x| {
                 let mut temp: [u8; 64] = [0; 64];
@@ -47,6 +61,7 @@ impl Sha256 {
             })
             .collect();
 
+        // for each chunk
         data.iter().for_each(|x| Self::process_64_byte(*x, &mut h));
 
         let mut result: [u8; 32] = [0; 32];
@@ -59,11 +74,17 @@ impl Sha256 {
     }
 
     fn process_64_byte(input: [u8; 64], h: &mut [u32; 8]) {
+
+        // create a 64-entry message schedule array w[0..64] of 32-bit words
+        // (The initial values in w[0..64] don't matter, so many implementations zero them here)
+        // copy chunk into first 16 words w[0..16] of the message schedule array
         let w: [[u8; 4]; 16] = input.as_chunks::<4>().0.try_into().unwrap();
         let temp = w.map(u32::from_be_bytes);
         let mut w: [u32; 64] = [0; 64];
         w[0..16].copy_from_slice(&temp);
 
+        // Extend the first 16 words into the remaining
+        // 48 words w[16..64] of the message schedule array:
         for x in 16..64 {
             let s0 = Self::right_rotate(w[x - 15], 7)
                 ^ Self::right_rotate(w[x - 15], 18)
@@ -77,8 +98,11 @@ impl Sha256 {
                 .wrapping_add(s1);
         }
 
-        let mut work_var: [u32; 8] = [0; 8]; // a b c d e f g h
+        // Initialize working variables to current hash value:
+        let mut work_var: [u32; 8] = [0; 8];
         work_var.copy_from_slice(h);
+
+        // Compression function main loop:
         for y in 0..64 {
             let s0 = Self::right_rotate(work_var[0], 2)
                 ^ Self::right_rotate(work_var[0], 13)
@@ -102,6 +126,8 @@ impl Sha256 {
             work_var[0] = temp1.wrapping_add(temp2);
             work_var[4] = work_var[4].wrapping_add(temp1);
         }
+
+        // Add the compressed chunk to the current hash value:
         for y in 0..8 {
             h[y] = h[y].wrapping_add(work_var[y]);
         }
@@ -118,14 +144,13 @@ mod tests {
 
     #[test]
     fn short_sha_256() {
-        let mut test = Sha256 {};
         assert_eq!(
-            test.do_hash(b"abc").to_vec(),
+            b"abc".to_vec().do_hash().to_vec(),
             hex::decode("ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad")
                 .unwrap()
         );
         assert_eq!(
-            test.do_hash(b"blockchain").to_vec(),
+            b"blockchain".to_vec().do_hash().to_vec(),
             hex::decode("ef7797e13d3a75526946a3bcf00daec9fc9c9c4d51ddc7cc5df888f74dd434d1")
                 .unwrap()
         );
@@ -133,16 +158,18 @@ mod tests {
 
     #[test]
     fn long_sha_256() {
-        let mut test = Sha256 {};
         assert_eq!(
-            test.do_hash(b"The quick brown fox jumps over the lazy dog")
+            b"The quick brown fox jumps over the lazy dog"
+                .to_vec()
+                .do_hash()
                 .to_vec(),
             hex::decode("d7a8fbb307d7809469ca9abcb0082e4f8d5651e46d3cdb762d02d0bf37c9e592")
                 .unwrap()
         );
         assert_eq!(
-            test
-                .do_hash(b"It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.")
+            b"It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife."
+                .to_vec()
+                .do_hash()
                 .to_vec(),
             hex::decode("ff3583336f59ddbfb6b202e15e9ed7fa05f3b6da8e159cd237af89fc02e5932e")
                 .unwrap()
