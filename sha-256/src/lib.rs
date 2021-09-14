100d65975bb0e19d8ecfe5520e09daf279dcc525
RalXYZ <RalXYZ@pm.me>
2021-09-14 11:14:21 +0800

    fix all bug in sha-256

--- sha-256/src/lib.rs
+++ sha-256/src/lib.rs
@@ -1,6 +1,7 @@
 #![feature(slice_as_chunks)]
 
 use std::convert::TryInto;
+use std::ops::{Div, Rem};
 
 const K: [u32; 64] = [
     0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
@@ -13,9 +14,7 @@ const K: [u32; 64] = [
     0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
 ];
 
-pub struct Sha256 {
-
-}
+pub struct Sha256 {}
 
 impl Default for Sha256 {
     fn default() -> Self {
@@ -24,48 +23,54 @@ impl Default for Sha256 {
 }
 
 impl Sha256 {
-    pub fn do_hash(&mut self, input: &[u8]) -> [u8; 32] {
+    pub fn do_hash(&mut self, data: &[u8]) -> [u8; 32] {
         let mut h: [u32; 8] = [
-            0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19,
+            0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab,
+            0x5be0cd19,
         ];
 
-        let input_len = input.len();
-        let mut pending: [u8; 64] = [0; 64];
-        pending[..input_len]
-            .copy_from_slice(&input); // FIXME: len greater than 64
-        pending[input_len] = 0b1000_0000;
-        pending[64 - 8..64]
-            .copy_from_slice(
-                &(input_len * 8)
-                    .to_be_bytes()
-            );
-        Self::process_64_byte(pending, &mut h);
+        let input_len = data.len();
+        let mut data: Vec<u8> = data.to_vec();
+        data.push(0b1000_0000);
+        if data.len().rem(64).lt(&56) {
+            data.append(&mut vec![0u8; 56 - (data.len().rem(64))]);
+        } else {
+            data.append(&mut vec![0u8; 64 + 56 - (data.len().rem(64))]);
+        }
+        data.extend_from_slice(&(input_len * 8).to_be_bytes());
+
+        let data: Vec<[u8; 64]> = (0..(data.len().div(64)))
+            .map(|x| {
+                let mut temp: [u8; 64] = [0; 64];
+                temp.copy_from_slice(&data[x * 64..x * 64 + 64]);
+                temp
+            })
+            .collect();
+
+        data.iter().for_each(|x| Self::process_64_byte(*x, &mut h));
 
         let mut result: [u8; 32] = [0; 32];
 
         for x in 0..8 {
-            result[x * 4 .. x * 4 + 4].copy_from_slice(&h[x].to_be_bytes());
+            result[x * 4..x * 4 + 4].copy_from_slice(&h[x].to_be_bytes());
         }
 
         result
     }
 
     fn process_64_byte(input: [u8; 64], h: &mut [u32; 8]) {
-        let w: [[u8; 4]; 16] = input.
-            as_chunks::<4>()
-            .0
-            .try_into()
-            .unwrap();
-        let temp = w
-            .map(u32::from_be_bytes);
+        let w: [[u8; 4]; 16] = input.as_chunks::<4>().0.try_into().unwrap();
+        let temp = w.map(u32::from_be_bytes);
         let mut w: [u32; 64] = [0; 64];
         w[0..16].copy_from_slice(&temp);
 
         for x in 16..64 {
             let s0 = Self::right_rotate(w[x - 15], 7)
-                ^ Self::right_rotate(w[x - 15], 18) ^ (w[x - 15] >> 3);
+                ^ Self::right_rotate(w[x - 15], 18)
+                ^ (w[x - 15] >> 3);
             let s1 = Self::right_rotate(w[x - 2], 17)
-                ^ Self::right_rotate(w[x - 2], 19) ^ (w[x - 2] >> 10);
+                ^ Self::right_rotate(w[x - 2], 19)
+                ^ (w[x - 2] >> 10);
             w[x] = w[x - 16]
                 .wrapping_add(s0)
                 .wrapping_add(w[x - 7])
@@ -75,20 +80,17 @@ impl Sha256 {
         let mut work_var: [u32; 8] = [0; 8]; // a b c d e f g h
         work_var.copy_from_slice(h);
         for y in 0..64 {
-            let s0 =
-                Self::right_rotate(work_var[0], 2)
-                    ^ Self::right_rotate(work_var[0], 13)
-                    ^ Self::right_rotate(work_var[0], 22);
-            let maj =
-                (work_var[0] & work_var[1])
-                    ^ (work_var[0] & work_var[2])
-                    ^ (work_var[1] & work_var[2]);
+            let s0 = Self::right_rotate(work_var[0], 2)
+                ^ Self::right_rotate(work_var[0], 13)
+                ^ Self::right_rotate(work_var[0], 22);
+            let maj = (work_var[0] & work_var[1])
+                ^ (work_var[0] & work_var[2])
+                ^ (work_var[1] & work_var[2]);
             let temp2 = s0.wrapping_add(maj);
             let s1 = Self::right_rotate(work_var[4], 6)
                 ^ Self::right_rotate(work_var[4], 11)
                 ^ Self::right_rotate(work_var[4], 25);
-            let ch = (work_var[4] & work_var[5])
-                ^ ((!work_var[4]) & work_var[6]);
+            let ch = (work_var[4] & work_var[5]) ^ ((!work_var[4]) & work_var[6]);
             let temp1 = work_var[7]
                 .wrapping_add(s1)
                 .wrapping_add(ch)
@@ -111,24 +113,19 @@ impl Sha256 {
 }
 
 #[cfg(test)]
-
 mod tests {
     use crate::Sha256;
 
     #[test]
     fn short_sha_256() {
-        let mut test = Sha256{};
+        let mut test = Sha256 {};
         assert_eq!(
-            test
-                .do_hash(b"abc")
-                .to_vec(),
+            test.do_hash(b"abc").to_vec(),
             hex::decode("ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad")
                 .unwrap()
         );
         assert_eq!(
-            test
-                .do_hash(b"blockchain")
-                .to_vec(),
+            test.do_hash(b"blockchain").to_vec(),
             hex::decode("ef7797e13d3a75526946a3bcf00daec9fc9c9c4d51ddc7cc5df888f74dd434d1")
                 .unwrap()
         );
@@ -136,10 +133,9 @@ mod tests {
 
     #[test]
     fn long_sha_256() {
-        let mut test = Sha256{};
+        let mut test = Sha256 {};
         assert_eq!(
-            test
-                .do_hash(b"The quick brown fox jumps over the lazy dog")
+            test.do_hash(b"The quick brown fox jumps over the lazy dog")
                 .to_vec(),
             hex::decode("d7a8fbb307d7809469ca9abcb0082e4f8d5651e46d3cdb762d02d0bf37c9e592")
                 .unwrap()
